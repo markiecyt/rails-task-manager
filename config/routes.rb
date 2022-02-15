f088b5bf3292d5e95109e946725e167553a35417
Markie Choi <markie054@gmail.com>
2022-02-15 13:22:38 +0900

    updated create new task

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get '/tasks/new', to: 'tasks#new', as: :new_task
+  post '/tasks', to: 'tasks#create'
   get '/tasks', to: 'tasks#index'
   get '/tasks/:id', to: 'tasks#show', as: :task
 end
